8c447c478d6f912b69c7974b59eae69bee28aa4f
Bjarni Jóhan Vihjálmsson <bjarni.vilhjalmsson@gmail.com>
2016-08-03 10:54:47 +0200

    working on fixing the bug

--- ancestor/core/ancestry.py
+++ ancestor/core/ancestry.py
@@ -177,7 +177,7 @@ def calc_genot_pcs(genot_file, pc_weights_dict, pc_stats, populations_to_use = [
     log.info('Loading genotypes')
     h5f = h5py.File(genot_file, 'r')
 
-    populations = h5f['indivs']['populations'][...]
+    populations = h5f['indivs']['ancestry'][...]
 #     populations = h5f['indivs']['continent'][...]  #Currently this is using continents, but we can/should switch to populations.  
     #We should also consider to allow us to combine multiple populations in one group, or something like that. 
     
